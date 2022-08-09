686788ca78501531aee53f877f6ea6987db070b2
Itamar da Silva Soares <Itamar_Soares@zohomail.com>
2022-08-09 17:47:14 -0300

    requisito 11 projeto trivia concluido e fix pagina Game

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,23 +5,28 @@ import { activateNextButtonAction,
   decrementTimeAction, toggleDisableAction } from '../redux/actions';
 
 class Timer extends React.Component {
-  componentDidMount() {
-    const ONE_SECOND = 1000;
-    const THIRTY_SECONDS = 30000;
-    const { decrementTime, disableButtons } = this.props;
-    this.interval = setInterval(() => {
-      decrementTime();
-    }, ONE_SECOND);
-
-    this.timeOut = setTimeout(() => {
-      disableButtons();
-    }, THIRTY_SECONDS);
-  }
-
   componentDidUpdate() {
     const { statePlayer: { timer } } = this.props;
+    const MAX_TIMER = 30;
     if (timer === 0) {
       clearInterval(this.interval);
+    }
+    if (timer === MAX_TIMER) {
+      const MAX_INTERVAL = 99999;
+      const intervalId = window.setInterval(() => {}, MAX_INTERVAL);
+      for (let number = 0; number < intervalId; number += 1) {
+        window.clearInterval(number);
+      }
+      const ONE_SECOND = 1000;
+      const THIRTY_SECONDS = 30000;
+      const { decrementTime, disableButtons } = this.props;
+      this.interval = setInterval(() => {
+        decrementTime();
+      }, ONE_SECOND);
+
+      this.timeOut = setTimeout(() => {
+        disableButtons();
+      }, THIRTY_SECONDS);
     }
   }
 
